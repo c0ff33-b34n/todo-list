743149eeb8114cd1ebeb2aee2f00ad4578e6784c
Stuart Bowes <stuartbowes2000@gmail.com>
2020-09-08 10:46:39 +0100

    Add todo list, todo item and output basic properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  private list = new TodoList('Stu',
+                [new TodoItem('Go for run', true),
+                 new TodoItem('Build an Angular project', true),
+                 new TodoItem('Be a well established full stack developer')]);
+
+  get username(): string { return this.list.user; }
+
+  get itemCount(): number { return this.list.items.filter(item => !item.complete).length; }
 }
